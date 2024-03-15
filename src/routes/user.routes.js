2251f2f638c06c0775ff8c75e1537353ceec856b
krunal dhavle <krunaldhavle1999@gmail.com>
2024-03-15 01:54:42 +0000

    added new router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
 } from "../controllers/user.controller.js";
 
 import { upload } from "../middlewares/multer.middleware.js";
@@ -28,5 +35,23 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refreshtoken").post(refreshAccessToken);
+
+router.route("/changepassword").post(verifyJWT, changeCurrentPassword);
+
+router.route("/currentuser").post(verifyJWT, getCurrentUser);
+
+router.route("/updateaccount").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+router
+  .route("/update-cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/watchhistory").get(verifyJWT, getWatchHistory);
 
 export { router };
